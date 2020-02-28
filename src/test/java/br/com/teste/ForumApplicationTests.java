9feeeb71e350fd65cdd63f87b388e7d15e45dbdd
Rafael de Jesus Augusto <RafaelAugusto@mpsp.mp.br>
2020-02-28 15:35:29 -0300

    Reposório usado no curso de SpringBoot do Alura

---
+++ src/test/java/br/com/teste/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
